86925f1abc9cc1e7cd474138e6dd0f9b45502544
rhksrb99 <rhksrb99@gmail.com>
2022-07-10 15:37:27 +0900

    Initial Commit

---
+++ NewWordsDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewWordsDictionary
+//
+//  Created by 박관규 on 2022/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
